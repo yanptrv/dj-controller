7b72fb88e868b64c07ae189ab8d64b8c3af65277
krispetrov <petrov.kris03@gmail.com>
2021-06-03 08:45:04 +0300

    Added Arduino code for communication

--- arduinoCode/djcon/djcon.ino
+++ arduinoCode/djcon/djcon.ino
@@ -27,53 +27,55 @@ void ifchanged() {
 }
 
 void setup() {
-  pinMode(A0, INPUT);
+  pinMode(A8, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
+  Serial.setTimeout(1);
 }
 
 void loop() {
-  volume1 = analogRead(A0);
+  volume1 = analogRead(A8);
   volume2 = analogRead(A1);
   seek1 = analogRead(A2);
   seek2 = analogRead(A3);
-  volume1 = map(volume1, 0, 1023, 0, 100);
+  volume1 = map(volume1, 0, 1023, 100, 0);
   volume2 = map(volume2, 0, 1023, 0, 100);
   seek1 = map(seek1, 0, 1023, 0, 100);
   seek2 = map(seek2, 0, 1023, 0, 100);
   lcd.setCursor(0, 0);
   lcd.print("Volume: ");
-  lcd.setCursor(1, 0);
+  lcd.setCursor(0, 1);
   lcd.print(volume1);
-  lcd.setCursor(1, 1);
-  lcd.print(volume2);
   ifchanged();
   if (volumechanged1 == true) {
-    Serial.println("v1");
-    Serial.println(volume1);
+    Serial.println('a');
     volumechanged1 = false;
+    lcd.clear();
+    delay(0.2);
+    Serial.println(volume1);
   }
   if (volumechanged2 == true) {
-    Serial.println("v2");
-    Serial.println(volume2);
+    Serial.println('b');
     volumechanged2 = false;
+    lcd.clear();
+    delay(0.2);
+    Serial.println(volume2);
   }
   if (seekchanged1 == true) {
-    Serial.println("s1");
-    Serial.println(seek1);
+    Serial.println('c');
     seekchanged1 = false;
+    delay(0.2);
+    Serial.println(seek1);
   }
   if (seekchanged2 == true) {
-    Serial.println("s2");
-    Serial.println(seek2);
+    Serial.println('d');
     seekchanged2 = false;
+    delay(0.2);
+    Serial.println(seek2);
   }
-  
-  delay(100);
-  lcd.clear();
 
 }
