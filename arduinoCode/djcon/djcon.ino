4983cfa3ae13fe29f1246dd0d901393e0d1a4e9f
krispetrov <petrov.kris03@gmail.com>
2021-06-03 17:22:49 +0300

    Updated Arduino code

--- arduinoCode/djcon/djcon.ino
+++ arduinoCode/djcon/djcon.ino
@@ -6,6 +6,8 @@ int volume1, lastvolume1, volume2, lastvolume2;
 bool volumechanged1 = false, volumechanged2 = false;
 int seek1, lastseek1, seek2, lastseek2;
 bool seekchanged1 = false, seekchanged2 = false;
+int flag = 0;
+int i = 0;
 
 void ifchanged() {
   if (lastvolume1 != volume1) {
@@ -44,35 +46,58 @@ void loop() {
   seek2 = analogRead(A3);
   volume1 = map(volume1, 0, 1023, 100, 0);
   volume2 = map(volume2, 0, 1023, 0, 100);
-  seek1 = map(seek1, 0, 1023, 0, 100);
-  seek2 = map(seek2, 0, 1023, 0, 100);
-  lcd.setCursor(0, 0);
-  lcd.print("Volume: ");
-  lcd.setCursor(0, 1);
-  lcd.print(volume1);
+  seek1 = map(seek1, 0, 1023, 100, 0);
+  seek2 = map(seek2, 0, 1023, 100, 0);
+  if (flag == 0) {;
+    lcd.setCursor(0, 0);
+    lcd.print("DJCON =)");
+    lcd.setCursor(0, 1);
+    lcd.print("Made by: Yan,Aon");
+  }
+  else if (flag == 1) {
+    lcd.setCursor(0, 0);
+    lcd.print("Volume 1: ");
+    lcd.setCursor(0, 1);
+    lcd.print(volume1);
+  }
+  else if (flag == 2) {
+    lcd.setCursor(0, 0);
+    lcd.print("Volume 2: ");
+    lcd.setCursor(0, 1);
+    lcd.print(volume2);
+  }
   ifchanged();
   if (volumechanged1 == true) {
-    Serial.println('a');
+    Serial.println("vol1");
     volumechanged1 = false;
-    lcd.clear();
     delay(0.2);
     Serial.println(volume1);
+    if (i > 1) {
+          flag = 1;
+    }
+    lcd.clear();
+    ++i;
   }
   if (volumechanged2 == true) {
-    Serial.println('b');
+    Serial.println("vol2");
     volumechanged2 = false;
     lcd.clear();
     delay(0.2);
     Serial.println(volume2);
+    if (i > 1) {
+          flag = 2;
+    }
+    lcd.clear();
+    ++i;
   }
   if (seekchanged1 == true) {
-    Serial.println('c');
+    Serial.println("seek1");
     seekchanged1 = false;
     delay(0.2);
     Serial.println(seek1);
   }
   if (seekchanged2 == true) {
-    Serial.println('d');
+    Serial.println("seek2");
     seekchanged2 = false;
     delay(0.2);
     Serial.println(seek2);
